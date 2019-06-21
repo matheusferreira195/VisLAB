117658f74bf538f2367320e96c7a111d6da772c2
Matheus Ferreira <matheus.ferreira@det.ufc.br>
2019-06-21 15:53:39 -0300

    added horizontal menu placeholders

--- as_0.1.py
+++ as_0.1.py
@@ -2,15 +2,53 @@ import os
 import pandas as pd
 import numpy as np
 import tkinter as tk
+from tkinter import *
 import win32com.client as com
+
+root = tk.Tk()
 
 
 #Inicialização do VISSIM
-Vissim = com.Dispatch("Vissim.Vissim") #abre o vissim
-path = os.path.join(os.getcwd(), 'test.inpx') #seta o diretorio do .inpx
-Vissim.LoadNet(path, False) #carrega a rede no vissim
 
-#
+'''Vissim = com.Dispatch("Vissim.Vissim") #abre o vissim
+
+path = os.path.join(os.getcwd(), 'test.inpx') #seta o diretorio do .inpx
+
+Vissim.LoadNet(path, False) #carrega a rede no vissim
+'''
+#Barra horizontal menu
+
+
+def donothing():
+   filewin = Toplevel(root)
+   button = Button(filewin, text="Do nothing button")
+   button.pack()
+
+menubar = Menu(root)
+
+menubar.add_command(label="Open", command=donothing)
+
+menubar.add_command(label="Save", command=donothing)
+
+menubar.add_command(label="Save as...", command=donothing)
+
+options_menu = Menu(menubar, tearoff = 0)
+menubar.add_cascade(label="Options", menu = options_menu)
+
+sa_menu = Menu(menubar, tearoff = 0)
+menubar.add_cascade(label="Sensitivity Analysis", menu = sa_menu)
+
+calib_menu = Menu(menubar, tearoff = 0)
+menubar.add_cascade(label="Calibration", menu = calib_menu)
+
+val_menu = Menu(menubar, tearoff = 0)
+menubar.add_cascade(label="Validation", menu = val_menu)
+
+help_menu = Menu(menubar, tearoff = 0)
+menubar.add_cascade(label="Help", menu = help_menu)
+
+root.config(menu=menubar)
+root.mainloop()
 
 
 
