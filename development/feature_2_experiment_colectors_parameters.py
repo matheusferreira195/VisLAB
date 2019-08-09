c5db8422bed9dee8b4ef99e53ddd443dbeabe1f9
matheus <matheus.ferreira@det.ufc.br>
2019-08-08 21:29:16 -0300

    changed names to better match the atual function of the pieces

--- development/feature_2_experiment_colectors_parameters.py
+++ development/feature_2_experiment_colectors_parameters.py
@@ -1,5 +1,8 @@
-#This file contains the net search method
+#This file contains the field that will allow the user to select evaluation points and variables
+
+
 import pandas as pd, win32com.client as com, win32gui, os, win32con
+from tkinter import *
 
 #Opens Vissim and load the net data
 Vissim = com.Dispatch("Vissim.Vissim")
@@ -23,4 +26,25 @@ traveltime_collectors = Vissim.Net.VehicleTravelTimeMeasurements.GetMultipleAttr
 print(data_collectors)
 print(queue_counters)
 print(traveltime_collectors)
-Vissim = None
+
+
+
+class Experiment:
+    pass
+
+class Colectors(Experiment):
+    def __init__ (self, object_type, name, no, perf_variable, time_interval):
+        self.object_type = object_type
+        self.name = name
+        self.no = no
+        self.perf_variable = perf_variable
+        self.time_interval = time_interval
+
+class Parameters(Experiment):
+    def __init__ (self, parameter, inf_lim, sup_lim, step):
+        self.parameter = parameter
+        self.inf_lim = inf_lim
+        self.sup_lim = sup_lim
+        self.step = step
+
+        
