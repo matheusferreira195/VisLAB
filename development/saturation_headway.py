f694c8a8e47642d6890974fff55dc7c182194738
chuqnorris10 <matheus.ferreira@det.ufc.br>
2019-10-07 21:45:52 -0300

    solved 2 TODOS: fixed replication handling for hdw

--- development/saturation_headway.py
+++ development/saturation_headway.py
@@ -12,74 +12,93 @@ from os.path import isfile, join
 import glob
 import os
 
-#TODO encapsular codigo em funcao que dado um diretorio e um data collector, calcula o sheadway
+def formatting(tipe, path):
+    skipline = 0
+    
+    if tipe == '.mer':
+        keyword = ' Measurem.'
+        
+    elif tipe == '.lsa':
+        keyword = 'SC'
+    
+    with open(path) as f:
+        
+        lines = f.readlines()
+        
+        for chunk in enumerate(lines):
+            if keyword in chunk[1]:
+                
+                if tipe == '.lsa':
+                
+                    skipline = int(chunk[0] + 2)
+                    
+                else:
+                    skipline = int(chunk[0])
+        #print(skipline)    
+        return skipline
+    
 
-def calculate_shdwy(path, dc, dc_number,sig_number):
+def calculate_shdwy(path, dc):
     lsa_columns = ['SimSec', 'CycleSec', 'SC', 'SG', 'Aspect', 'Prev', 'Crit', 'duetoSG']
     os.chdir(path)
     mers = [file for file in glob.glob("*.mer")]
     lsas = [file for file in glob.glob("*.lsa")]
-
+    headways_df = pd.DataFrame(columns=['Replication','Cicle','Position','Headway'])
+    #print(mers)
+    #print(len(mers))
     for i in range(len(mers)):
 
-        mer_data_raw = pd.read_csv(mers[i], sep=';', skiprows=(9+dc_number), skipinitialspace=True, index_col=False) 
+        print(mers[i])
+
+        mer_data_raw = pd.read_csv(mers[i], sep=';', skiprows=formatting('.mer',mers[i]), skipinitialspace=True, index_col=False) 
         mer_data = mer_data_raw.apply(lambda x: x.str.strip() if x.dtype == 'object' else x)
-
-        lsa_data_raw = pd.read_csv(lsas[i], sep=';', skiprows=(9+sig_number), names=lsa_columns, skipinitialspace=True, index_col=False)
+        #print(mer_data_raw)
+        lsa_data_raw = pd.read_csv(lsas[i], sep=';', skiprows=formatting('.lsa',lsas[i]), names=lsa_columns, skipinitialspace=True, index_col=False)
         lsa_data = lsa_data_raw.apply(lambda x: x.str.strip() if x.dtype == 'object' else x)
-
+        #print(lsa_data_raw)
         green_windows = lsa_data[(lsa_data['Aspect'] == 'green') & (lsa_data['Aspect']=='red')]['SimSec']
         raw_green_windows = lsa_data.query('Aspect in list(["green", "red"])').reset_index(drop=True)
-
 
         cleaned_mer = mer_data[(mer_data['t(Entry)'] !=0 )]     
         
         green_windows=[]
         interval=[]
-        headways_df = pd.DataFrame(columns=['Cicle','Position','Headway'])
+        
+        for w in range(len(raw_green_windows['SimSec']) -1):
 
-
-        for i in range(len(raw_green_windows['SimSec']) -1):
-
-            if raw_green_windows['Aspect'][i] == 'green':
+            if raw_green_windows['Aspect'][w] == 'green':
                     
-                interval = [raw_green_windows['SimSec'][i],raw_green_windows['SimSec'][i+1]]    
+                interval = [raw_green_windows['SimSec'][w],raw_green_windows['SimSec'][w+1]]    
                 green_windows.append(interval)
-                
-        for i in range(len(green_windows)):
+         
+        for j in range(len(green_windows)):
             
             headways_to_bar = []
 
             
-            df = cleaned_mer[(cleaned_mer['t(Entry)'] > green_windows[i][0]) 
-                            & (cleaned_mer['t(Entry)'] < green_windows[i][1]) 
+            df = cleaned_mer[(cleaned_mer['t(Entry)'] > green_windows[j][0]) 
+                            & (cleaned_mer['t(Entry)'] < green_windows[j][1]) 
                             & (cleaned_mer['tQueue'] != 0)
                             & (cleaned_mer['Measurem.'] == dc)]
             
             if not df.empty:
                 
-                #print(df)
                 
                 filtrados = list(df['t(Entry)'])
-                nao_filtrados = df['t(Entry)']
-                print(filtrados)
-                #print(nao_filtrados)
-            
-                for i in range(1,len(filtrados)):
-                    
-                    headway = filtrados[i] - filtrados[i-1]
-                    headways_dict = {'Cicle':green_windows[i],'Position':i, 'Headway': headway}
-                    headways_to_bar.append(headway)
-                    
-                    headways_df = headways_df.append(headways_dict, ignore_index = True)
 
-                x = list(range(1,len(filtrados)))
-                #print(x)
-                y = headways_to_bar
-                #print(y)
-                plt.bar(x,y)
-                plt.show()
-        print(headways_df)
+                if len(filtrados) >= 4:
+
+                    for k in range(1,len(filtrados)):
+                        
+                        headway = filtrados[k] - filtrados[k-1]
+                        headways_dict = {'Replication':i,'Cicle':green_windows[j],'Position':k, 'Headway': headway}
+                        headways_to_bar.append(headway)
+                        
+                        headways_df = headways_df.append(headways_dict, ignore_index = True)
+
+                
+    return headways_df 
         
 
-calculate_shdwy("E:\\Google Drive\Scripts\\vistools\\development\\net\\teste\\",1,5,2) #TODO pegar DC correto    
+h = calculate_shdwy("E:\\Google Drive\Scripts\\vistools\\development\\net\\teste\\",5) 
+print(h)
